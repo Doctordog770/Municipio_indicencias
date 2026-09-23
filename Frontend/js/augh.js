85a6f51a7c69149e3a0e9a0620ed5f7fe1362df6
aperkscdg <byrongameplayer@gmail.com>
2026-09-22 22:17:34 -0300

    register y login ya crean la cuenta y logean a las personas

--- Frontend/js/augh.js
+++ Frontend/js/augh.js
@@ -1,17 +1,16 @@
 import { fetch_endpoints } from "./fetch.js";
 
-const formulario = document.getElementById('form');
+const formulario = document.getElementById('formRegister');
 
 formulario.addEventListener('submit', async (e) => {
-    
+
     e.preventDefault();
 
-    var json = await fetch_endpoints(formulario,"../../api/augh/Sing_Up_Code.php");
+    var json = await fetch_endpoints(formulario, "../api/augh/Sing_Up_Code.php");
 
     if(json.ok){
-        document.getElementById("error").innerHTML = json.mensaje;
+        document.getElementById("errorRegister").innerHTML = json.mensaje;
     } else {
-        document.getElementById("error").innerHTML = json.mensaje;
+        document.getElementById("errorRegister").innerHTML = json.mensaje;
     }
 })
-
--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,18 +1,18 @@
 import { fetch_endpoints } from "./fetch.js";
 
-const formulario = document.getElementById("form");
+const formulario = document.getElementById("formLogin");
 
 formulario.addEventListener('submit', async (e) => {
-    
+
     e.preventDefault();
 
-    var json = await fetch_endpoints(formulario,"../../api/augh/Sing_in_Code.php");
+    var json = await fetch_endpoints(formulario, "../api/augh/Sing_in_Code.php");
 
     if(json.ok){
         localStorage.setItem('token', json.TOKEN);
         localStorage.setItem('rol', json.ROL);
-        document.getElementById("error").innerHTML = json.mensaje;
+        document.getElementById("errorLogin").innerHTML = json.mensaje;
     } else {
-        document.getElementById("error").innerHTML = json.mensaje;
+        document.getElementById("errorLogin").innerHTML = json.mensaje;
     }
 })
